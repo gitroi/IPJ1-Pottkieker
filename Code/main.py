69d3084f8e58f0124a0aa4d66119e0ae54fbef99
Joris <Joris.Buerger@haw-hamburg.de>
2025-11-04 18:48:01 +0100

    Anpassung/ Aufteilung der Analyse auf Dataframe und csv Datei

--- Code/main.py
+++ Code/main.py
@@ -5,17 +5,32 @@ import Analyse
 import Erzeugungsprognosen
 import Prognose_Verbrauch
 import Histogramme as Hist
+import EE_Anteil_DataFrame as EEdf
 
-# verbrauch = Prognose_Verbrauch.Prognose_Verbrauch(650e6, 1200e6)
-# erzeugung = Erzeugungsprognosen.Prognose_erzeugung(0.068, 0.045, 0.1, 0.01, 0, 0)
+verbrauch = Prognose_Verbrauch.Prognose_Verbrauch(650e6, 1200e6)
+erzeugung = Erzeugungsprognosen.Prognose_erzeugung(0.068, 0.045, 0.1, 0.01, 0, 0)
 
-verbrauch = pd.read_csv('C:\\Users\\joris\\OneDrive - HAW-HH\\Labore\\Integrationsprojekt1\\IPJ1-Pottkieker\\Daten\\Ist_Analyse\\Verbrauch.csv', sep=';', decimal=',')
-erzeugung = pd.read_csv('C:\\Users\\joris\\OneDrive - HAW-HH\\Labore\\Integrationsprojekt1\\IPJ1-Pottkieker\\Daten\\Ist_Analyse\\Erzeugung.csv', sep=';', decimal=',')
+gemerged = pd.merge(
+    erzeugung,
+    verbrauch,
+    on="Datum von",
+    how="inner",
+)   
 
-gesamt =Analyse.analyse_erneuerbare_anteil(
-    "C:\\Users\\joris\\OneDrive - HAW-HH\\Labore\\Integrationsprojekt1\\IPJ1-Pottkieker\\Daten\\Ist_Analyse\\erzeugung.csv",
-    "C:\\Users\\joris\\OneDrive - HAW-HH\\Labore\\Integrationsprojekt1\\IPJ1-Pottkieker\\Daten\\Ist_Analyse\\verbrauch.csv",
-    "Netzlast inkl. Pumpspeicher [MWh] Originalauflösungen"
+gemerged = gemerged.head()
+
+gemerged.to_excel(
+    "C:\\Users\\joris\\Documents\\IPJ1\\Daten\\Prognose_Verbrauch_Erzeugung.xlsx",
+    index=False, 
 )
 
-Hist.plot_ee_anteil_histogram_overflow(gesamt)
+gesamt2 = EEdf.anteil_erneuerbare_df(erzeugung, verbrauch, "Netzlast [MWh] Originalauflösungen")
+print(gesamt2.head())  
+
+# gesamt =Analyse.analyse_erneuerbare_anteil(
+#      "C:\\Users\\joris\\OneDrive - HAW-HH\\Labore\\Integrationsprojekt1\\IPJ1-Pottkieker\\Daten\\Ist_Analyse\\erzeugung.csv",
+#      "C:\\Users\\joris\\OneDrive - HAW-HH\\Labore\\Integrationsprojekt1\\IPJ1-Pottkieker\\Daten\\Ist_Analyse\\verbrauch.csv",
+#      "Netzlast inkl. Pumpspeicher [MWh] Originalauflösungen"
+# )
+
+# Hist.plot_ee_anteil_histogram_overflow(gesamt2)
