14f4c41ee2ec7ab575911210859f6ea52313d737
Robin Matzke <robin.matzke@gmx.de>
2025-11-14 14:47:00 +0100

    "import os" rausgenommen, da nicht nötig mit pathlib

--- Code/Prognose_Verbrauch.py
+++ Code/Prognose_Verbrauch.py
@@ -7,7 +7,6 @@ Nutzt Daten aus 2024 um eine Prognose bis 2045 zu erstellen.
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import os
 from config import PROJECT_ROOT 
 
 def Prognose_Verbrauch(verbrauch_2030_TWh , verbrauch_2045_TWh):
