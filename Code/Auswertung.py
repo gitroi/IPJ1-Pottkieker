e56e481456c4a38eef9a914bbf1d3ae14c862723
Joris <Joris.Buerger@haw-hamburg.de>
2025-12-02 21:05:33 +0100

    Auswertung für ein Szenario angepasst

--- Code/Auswertung.py
+++ Code/Auswertung.py
@@ -25,18 +25,27 @@ def ausgabe(kosten_df: pd.DataFrame, ee_df: pd.DataFrame,szenario: json):
     jahre = range(2026, 2046)
     end_df = pd.DataFrame({"Jahr": jahre})
 
-    for jahr in range(2026, 2046):
-        end_df.loc[end_df["Jahr"] == jahr, "Realisierte Erzeugung [TWh]"] = gesamt[gesamt["Jahr"] == jahr]["Realisierte Erzeugung [MWh]"].sum().round(2) / 1e6
-        end_df.loc[end_df["Jahr"] == jahr, "Erzeugung Erneuerbare [TWh]"] = gesamt[gesamt["Jahr"] == jahr]["Erneuerbare [MWh]"].sum().round(2) / 1e6
-        end_df.loc[end_df["Jahr"] == jahr, "Verbrauch [TWh]"] = gesamt[gesamt["Jahr"] == jahr]["Netzlast [MWh]"].sum().round(2) / 1e6
-        end_df.loc[end_df["Jahr"] == jahr, "Anteil ohne Speicher mit 100% [%]"] = round(gesamt[(gesamt["Jahr"] == jahr) & (gesamt["Anteil Erneuerbare [%]"] >= 100)].shape[0] / len(gesamt[gesamt["Jahr"] == jahr]) * 100, 2)
-        end_df.loc[end_df["Jahr"] == jahr, "Anteil mit Speicher mit 100% [%]"] = round(gesamt[(gesamt["Jahr"] == jahr) & (gesamt["Anteil Erneuerbare Speicher [%]"] >= 100)].shape[0] / len(gesamt[gesamt["Jahr"] == jahr]) * 100, 2)
-        for key in Keys_Erzeugung:
-            end_df.loc[end_df["Jahr"] == jahr, f"Gesamtkosten {key} [Tsd. €]"] = gesamt[gesamt["Jahr"] == jahr][f"Gesamtkosten {key} [€]"].sum() / 1e3
-    
+    gesamt["Jahr"] = gesamt["Datum von"].dt.year
+
+    end_df['Energie aus Speichern [TWh]'] = (gesamt.groupby('Jahr')['Energie aus Speicher [MWh]'].sum() / 1e6).round(2).values
+    end_df['Erzeugung Erneuerbare [TWh]'] = (gesamt.groupby('Jahr')['Erneuerbare [MWh]'].sum() / 1e6).round(2).values
+    end_df['Verbrauch [TWh]'] = (gesamt.groupby('Jahr')['Netzlast [MWh]'].sum() / 1e6).round(2).values
+
+    nur_100_ohne = gesamt[gesamt["Anteil Erneuerbare [%]"] >= 100]
+    nur_100_mit = gesamt[gesamt["Anteil Erneuerbare Speicher [%]"] >= 100]
+
+    anzahl_100_ohne = nur_100_ohne.groupby('Jahr').size()
+    anzahl_100_mit = nur_100_mit.groupby('Jahr').size()
+    anzahl_gesamt = gesamt.groupby('Jahr').size()
+
+    end_df['Anteil ohne Speicher mit 100% [%]'] = (anzahl_100_ohne / anzahl_gesamt * 100).round(2).values
+    end_df['Anteil mit Speicher mit 100% [%]'] = (anzahl_100_mit / anzahl_gesamt * 100).round(2).values
+
     for key in Keys_Erzeugung:
-        end_df.loc[end_df["Jahr"]<=2030, f"Ausbaurate {key} [GW/Jahr]"] = round(ausbauraten["zuwachsrate_2030"][key], 2)
-        end_df.loc[end_df["Jahr"]>2030, f"Ausbaurate {key} [GW/Jahr]"] = round(ausbauraten["zuwachsrate_2045"][key], 2)
+        kosten_summe = gesamt.groupby('Jahr')[f"Gesamtkosten {key} [€]"].sum() / 1e3
+        end_df[f"Gesamtkosten {key} [Tsd. €]"] = kosten_summe.values
+
+    end_df["Gesamtkosten_EE [Tsd. €]"] = (gesamt.groupby('Jahr')["Gesamtkosten_EE [€]"].sum() / 1e3).round(2).values
 
     with pd.ExcelWriter(pfad, engine='openpyxl') as writer:
         end_df.to_excel(writer, sheet_name="Jahresübersicht", index=False)
